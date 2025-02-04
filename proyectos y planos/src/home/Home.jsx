ba7cc6215f1107e1ac72bd2865ff7b9878e36e40
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2025-02-03 22:31:59 -0300

    add vite project

---
+++ proyectos y planos/src/home/Home.jsx
@@ -0,0 +1,26 @@
+
+function Home() {
+
+    return (
+        <>
+            <div className="overlay">
+                <section className="home">
+                    <div className="content">
+                        <h1>Planos Y<br />Proyectos</h1>
+                        <h3>MAESTRO MAYOR DE OBRAS</h3>
+                    </div>
+                    <h1 class="text-4xl font-bold underline">
+                        Hello world!
+                    </h1>
+                    <div className="image-gallery">
+                        <img src="images/home1.png" alt="" />
+                        <img src="images/frentes.jpg" alt="" />
+                        <img src="images/GONZALEZ-MITRE.jpg" alt="" />
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
+
+export default Home
