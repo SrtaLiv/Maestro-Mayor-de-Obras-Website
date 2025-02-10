24eba6a39bad13bb7d3869ff94f458267fcef265
Sofía Todesco <166171810+Arimochin@users.noreply.github.com>
2025-02-09 23:52:49 -0300

    navbar looks nice in desktop c':

--- src/projects/NavBar.jsx
+++ src/projects/NavBar.jsx
@@ -3,12 +3,16 @@ import './navbar.css'
 function NavBar() {
     return (
         <>
-            <div className="px-8 py-4 flex justify-between content-center bg-black " >
+            <div className="fixed top-0 w-full z-50 flex justify-between content-center bg-black " >
                 <img className="nav-logo" src="/src/assets/images/LOGO.png" />
-                <h1 className="nav-text">INICIO</h1>
-                <h1 className="nav-text">PROYECTOS</h1>
-                <h1 className="nav-text">SERVICIOS</h1>
-                <h1 className="nav-text">CONTACTOS</h1>
+                <div className="flex  content-center">
+                    <h1 className="nav-text">INICIO</h1>
+                    <h1 className="nav-text">PROYECTOS</h1>
+                    <h1 className="nav-text">SERVICIOS</h1>
+                    <h1 className="nav-text">CONTACTOS</h1>
+
+                </div>
+
             </div>
             
         </>
