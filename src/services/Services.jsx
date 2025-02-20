b40a67980e1a6a1fd82edb71c819c95769831806
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2025-02-20 00:13:14 -0300

    add colors and improve borders

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -35,15 +35,14 @@ function Services() {
                         icon: Building2,
                     },
                 ].map((service) => (
-                    <Card backgroundColor="[#1080A0]" className="border-gray-100 border-solid
-  bg-black services-card h-full w-full px-7 group hover:scale-105 transition-transform duration-300 ease-in-out hover:shadow-lg">
+                    <Card backgroundColor="[#1a1a1a]"  className="border-finito border-[1px] h-full w-full px-7 group hover:scale-105 transition-transform duration-300 ease-in-out hover:shadow-lg">
                         <CardContent className="pt-6">
                             <div className="flex justify-center mb-6">
-                                <service.icon className="w-16 h-16 text-[#1080A0]" />
+                                <service.icon className="w-16 h-16 text-orange-400" />
                             </div>
                             <h3 className="text-white text-xl font-semibold mb-1">{service.title}</h3>
                             <p className="text-gray-300 text-muted-foreground mb-6">{service.description}</p>
-                            <Button>Saber mas</Button> 
+                            <Button className="btn-service bg-orange-400">Más informacion</Button>
                         </CardContent>
                     </Card>
                 ))}
